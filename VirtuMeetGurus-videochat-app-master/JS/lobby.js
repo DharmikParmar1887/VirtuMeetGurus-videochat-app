4eb5e0b7c175abae4a6c4fcb039d370a5c09df9d
DharmikParmar1887 <140688528+DharmikParmar1887@users.noreply.github.com>
2023-11-01 21:45:18 +0530

    Add files via upload

---
+++ VirtuMeetGurus-videochat-app-master/JS/lobby.js
@@ -0,0 +1,101 @@
+
+let displayName = sessionStorage.getItem('display_name');
+let roomID = sessionStorage.getItem('roomID');
+let meetingLink = '';
+
+if (displayName) {
+    form.namespaceURI.value = displayName;
+}
+
+if (!displayName && roomID != 'null') {
+
+    document.getElementById('generate_field').remove();
+    document.getElementById('or_span').remove();
+    document.getElementById('room_id_field').remove();
+    document.getElementById('copyText').remove();
+    document.getElementById('lobby__form').style.height = '200px';
+}
+
+let form = document.getElementById('lobby__form');
+
+form.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    sessionStorage.setItem('display_name', e.target.name.value);
+
+    let inviteCode = '';
+    if (roomID != 'null') {
+
+        meetingLink += `https://VirtuMeetGurus.web.app/index.html?room=${roomID}`;
+        window.location = `index.html?room=${roomID}`;
+
+    } else if (roomID == 'null') {
+
+        inviteCode = e.target.room.value;
+        if (!inviteCode) {
+            let string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+            inviteCode = '';
+
+            for (let i = 0; i < 3; i++) {
+
+                let index1 = Math.floor(Math.random() * 61 + 0);
+                let index2 = Math.floor(Math.random() * 61 + 0);
+                let index3 = Math.floor(Math.random() * 61 + 0);
+                if (i != 2) {
+                    inviteCode += `${string[index1]}${string[index2]}${string[index3]}-`;
+                } else {
+                    inviteCode += `${string[index1]}${string[index2]}${string[index3]}`;
+                }
+            }
+        }
+
+        meetingLink += `https://VirtuMeetGurus.web.app/index.html?room=${inviteCode}`;
+
+        window.location = `index.html?room=${inviteCode}`;
+    }
+
+});
+
+
+
+let generateUniqueRoomId = () => {
+
+    let roomIDInputField = document.getElementById('lobby_room_id_btn');
+    let string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    newlyGeneratedId = '';
+
+    for (let i = 0; i < 3; i++) {
+
+        let index1 = Math.floor(Math.random() * 61 + 0);
+        let index2 = Math.floor(Math.random() * 61 + 0);
+        let index3 = Math.floor(Math.random() * 61 + 0);
+
+        if (i != 2) {
+            newlyGeneratedId += `${string[index1]}${string[index2]}${string[index3]}-`;
+        } else {
+            newlyGeneratedId += `${string[index1]}${string[index2]}${string[index3]}`;
+        }
+    }
+
+    roomIDInputField.value = newlyGeneratedId;
+    navigator.clipboard.writeText(newlyGeneratedId);
+
+    let copyText = document.getElementById('copyText');
+    copyText.style.top = '0';
+
+    let lobbyForm = document.getElementById('lobby__form');
+    lobbyForm.style.height = '465px';
+
+    setTimeout(() => {
+        copyText.style.top = '25px';
+        lobbyForm.style.height = '427px';
+    }, 3000);
+
+}
+
+let generateMeetingLink = async () => {
+
+}
+
+document.getElementById('lobby_generate_room_id_btn').addEventListener('click', generateUniqueRoomId);
---
+++ VirtuMeetGurus-videochat-app-master/README.md
@@ -0,0 +1,13 @@
+# VirtuMeetGurus-videochat-app
+Inside this repository, you'll find the source code for VirtuMeetGurus, a video call and meeting app created with WebRTC technology and integrated with Agora.io services. VirtuMeetGurus empowers users to participate in high-quality, real-time video conversations, ensuring effortless virtual meetings, conferences, and interactive dialogues.
+#### Link to website : [VirtuMeetGurus](https://VirtuMeetGurus.web.app/)
+## WebRTC
+[WebRTC](https://www.webrtc.org) Web Real-Time Communication (WebRTC) is an open-source initiative facilitating real-time interaction between web browsers and mobile apps. It offers JavaScript APIs and protocols for integrating audio, video, and data sharing features into web applications, eliminating the need for extra software or plugins.
+## agora.io
+[Agora.io](https://www.agora.io) serves as a platform for instant communication, offering a range of APIs and SDKs to empower developers in adding voice, video, and interactive broadcasting features to their apps and services. It offers cloud-based infrastructure that manages the intricacies of real-time communication, enabling developers to concentrate on creating engaging and interactive user experiences.
+### How to get APP-ID from agora
+* Go to [agora.io](https://www.agora.io)
+* Sign Up or Login
+* Under My Projects table click on create a Project
+* Give your project name and select your use case (ex. Social/Chatroom) and copy your App Id
+* Paste your AppId in js/room_rtc.js
